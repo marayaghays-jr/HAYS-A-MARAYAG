35ffa5719cbcc5a93494388b2a29e9a6cab41907
marayaghays-jr <marayaghays5@gmail.com>
2025-10-16 22:13:48 +0800

    Add files via upload

---
+++ Task 1 The Basic Calculator.py
@@ -0,0 +1,12 @@
+# Assign two numbers to variables a and b
+a = 15
+b = 4
+
+# Perform and print each arithmetic operation
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Floating-Point Division:", a / b)
+print("Integer Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
